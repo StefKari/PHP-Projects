c068240434d4945bd53183f40cd7c3223231e68e
StefKari <stefan.karadzic55@gmail.com>
2020-02-12 00:40:31 +0100

    footer.php

---
+++ Monitor/admin/inc/footer.php
@@ -0,0 +1,13 @@
+
+    <footer id="footer">
+      <div class="container-fluid px-0">
+            <div class="row no-gutters">
+                <div class="col-12">
+                  <p>Copyright &copy; <a href="https://github.com/StefKari" target="_blank">github.com/StefKari</a> All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
