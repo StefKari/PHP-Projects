cb2de6d2f2a5cede63d69d05b28bee7aba95c410
StefKari <stefan.karadzic55@gmail.com>
2020-02-12 01:06:40 +0100

    manufacturerAdd.php

---
+++ ServisnaKnjiga/public/manufacturerAdd.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once '../config/Database.php';
+include_once '../classes/Manufacturer.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$manufacturer = new Manufacturer($db);
+
+$manufacturer->setName($_POST['name']);
+
+if($manufacturer->save()){
+    $user_arr=array(
+        "status" => true,
+        "message" => "Uspesno sacuvan!",
+        "id" => $manufacturer->getId(),
+        "manufacturer" => $manufacturer->getName()
+    );
+}
+else{
+    $user_arr=array(
+        "status" => false,
+        "message" => "Proizvodjac vec postoji!"
+    );
+}
+print_r(json_encode($user_arr));
+?>
