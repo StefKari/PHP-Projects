5e8a8e42fe24037ca1f65052ed35452e06ae485a
StefKari <stefan.karadzic55@gmail.com>
2020-02-12 01:05:54 +0100

    checkSession.php

---
+++ ServisnaKnjiga/public/checkSession.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once '../config/database.php';
+
+//$session = $_POST['id'];
+session_start();
+//if(isset($_SESSION['id']) && $_SESSION['id'] == $session)
+if(isset($_SESSION['id']))
+{
+
+    $arr = array(
+        'status' => true,
+        'name' => $_SESSION['username'],
+        'id' => $_SESSION['id']
+    );
+}
+else
+{
+    $arr = array(
+        'status' => false
+    );
+}
+
+print_r(json_encode($arr));
