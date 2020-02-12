979edef34ca190791a1806ec48d88b07da59ff7a
StefKari <stefan.karadzic55@gmail.com>
2020-02-12 01:04:36 +0100

    database.php

---
+++ ServisnaKnjiga/config/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database
+{
+
+    private $host="localhost";
+    private $username="root";
+    private $password="";
+    private $dbName="servisna_knjiga";
+
+    public $conn;
+
+    public function getConnection(){
+
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
