2a0e63c92bb7b549d39146cb0b610a5f04c7fef4
StefKari <stefan.karadzic55@gmail.com>
2020-02-12 01:08:37 +0100

    showManufactorers.php

---
+++ ServisnaKnjiga/public/showManufacturers.php
@@ -0,0 +1,17 @@
+<?php
+
+
+include_once '../config/Database.php';
+include_once '../classes/Manufacturer.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$manufacturer = new Manufacturer($db);
+
+$stmt = $manufacturer->findAll();
+
+$row = $stmt->fetchAll();
+
+print_r(json_encode($row));
+?>
