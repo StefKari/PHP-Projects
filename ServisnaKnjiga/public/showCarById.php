e4883edb4006d5fa67b6b49ccf7fff1b01883f38
StefKari <stefan.karadzic55@gmail.com>
2020-02-12 01:07:29 +0100

    showCarById.php

---
+++ ServisnaKnjiga/public/showCarById.php
@@ -0,0 +1,16 @@
+<?php
+
+
+include_once '../config/Database.php';
+include_once '../classes/Car.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$car = new Car($db);
+
+$stmt = $car->findCarById($_POST['id']);
+
+$row = $stmt->fetchAll();
+
+print_r(json_encode($row));
