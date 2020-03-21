99869723035885edf085572c78f24322686038bd
StefKari <59152638+StefKari@users.noreply.github.com>
2020-03-21 22:24:38 +0100

    Update post.php

--- Monitor/post.php
+++ Monitor/post.php
@@ -1,6 +1,6 @@
 <?php
 include "inc/header.php";
-include "inc/comment_add.php";
+include "inc/comment-add.php";
 
 
   /* Ucitavanje id posta i proverava da li postoji */
@@ -11,13 +11,12 @@ include "inc/comment_add.php";
   	$id = $_GET['id'];
   }
 
+
   /* Brisanje komentara preko unetog id $comment_id */
   if (isset($_POST['comment_id'])) {
-
-    $comment_id = $_POST['comment_id'];
+    $comment_id = mysqli_real_escape_string($database->link, $_POST['comment_id']);
 
     $comment_delete = "DELETE FROM comments WHERE id = '$comment_id' ";
-
     $del_comm = $database->delete($comment_delete);
 
       if ($del_comm) {
@@ -29,8 +28,8 @@ include "inc/comment_add.php";
     }
 
 ?>
+  <!-- Dodavanje sekcije post ispod su komentari -->
 	<div class="contentsection contemplete clear">
-    <?php include "inc/sidebar.php"; ?>
 		<div class="maincontent clear">
 			<div class="about">
 				<?php
@@ -48,119 +47,76 @@ include "inc/comment_add.php";
   					   }
     				}
   				?>
+
       <!-- Komentari -->
       <div class="demo-container">
-          <form action=" " id="frmComment" method="POST">
+          <form action="" id="frmComment" method="POST">
+            <?php
+              if(isset($error)) {
+                echo "<span style='color:#e00000;'>$error</span><br><br>";
+              }
+             ?>
             <div class="row">
-              <label> Name: </label> <span id="name-info"></span><input class="form-field" id="name"
-                type="text" name="name">
+              <label for="name">Name:</label>
+              <input class="form-field" id="name" type="text" name="name">
             </div>
             <div class="row">
-              <label for="mesg"> Message: <span id="message-info"></span></label>
+              <label for="message">Message:</label>
               <textarea class="form-field" id="message" name="message" rows="4"></textarea>
-
             </div>
             <div class="row">
-              <input type="hidden" name="add" value="post">
-              <button type="submit" name="submit" id="submit" class="btn-add-comment">Add Comment</button>
-              <img src="images/LoaderIcon.gif" id="loader">
+              <button type="submit" name="submit" id="submit" class="btn-add-comment">Add Comment...</button>
             </div>
           </form>
           <?php
 
             $query = "SELECT * FROM comments WHERE post_id = '$postid' ORDER BY id DESC";
             $comment_id = $database->select($query);
-          ?>
-            <div id="response">
-          <?php
-            if($comment_id) {
-            while($result = $comment_id->fetch_assoc()) {
 
           ?>
-            <div id="comment-<?php echo $result["id"];?>" class="comment-row">
-              <div class="comment-name"><i class='fas fa-user'></i> <?php echo $result["name"]; ?> <span class="comment-date"><?php echo $format->formatData($result["date"]);?></span></div>
-              <div class="comment-msg" id="msgdiv-<?php echo $result["id"];?>"><?php echo $result["message"];?></div>
-              <div class="delete" name="delete"
-                id="delete-<?php echo $result["id"];?>"
-                onclick="deletecomment(<?php echo $result['id'];?>)">Delete</div>
-            </div>
-            <?php
-                }
-              }
-            ?>
+            <div id="response">
+              <?php
+              if($comment_id) {
+                while($result = $comment_id->fetch_assoc()) {
+              ?>
+                <div id="comment<?php echo $result["id"];?>" class="comment-row">
+                  <div class="comment-name"><i class='fas fa-user'></i> <?php echo $result["name"]; ?> <span class="comment-date"><?php echo $format->formatData($result["date"]);?></span></div>
+                  <div class="comment-msg" id="msgdiv-<?php echo $result["id"];?>"><?php echo $result["message"];?></div>
+                  <div class="delete" name="delete"
+                    id="delete<?php echo $result["id"];?>"
+                    onclick="deletecomment(<?php echo $result['id'];?>)">Delete</div>
+                </div>
+                <?php
+                    }
+                  }
+                ?>
             </div>
           </div>
           <!-- Kraj Komentara -->
         </div>
       </div>
 
-
-      <script type="text/javascript"></script>
-    	<script>
+	<script type="text/javascript">
 
         function deletecomment(id) {
-
            if(confirm("Are you sure you want to delete this comment?")) {
-
                 $.ajax({
                 url: "post.php",
                 type: "POST",
                 data: 'comment_id='+id,
                 success: function(data){
-                    if (data)
-                    {
-                        $("#comment-"+id).remove();
-
+                    if (data) {
+                      $("#comment"+id).remove();
                     }
                 }
                });
             }
           }
 
-
-
-      	$(document).ready(function() {
-
-          ("#frmComment").on("submit", function(e) {
-          $(".error").text("");
-          $('#name-info').removeClass("error");
-          $('#message-info').removeClass("error");
-          e.preventDefault();
-          var name = $('#name').val();
-          var message = $('#message').val();
-
-          if(name == ""){
-              $('#name-info').addClass("error");
-          }
-          if(message == ""){
-              $('#message-info').addClass("error");
-          }
-          $(".error").text("required");
-          if(name && message){
-                $("#loader").show();
-              $("#submit").hide();
-              $.ajax({
-
-               type:'POST',
-               url: 'comment-add.php',
-               data: $(this).serialize(),
-               success: function(response)
-                  {
-          $("#frmComment input").val("");
-          $("#frmComment textarea").val("");
-                    $('#response').prepend(response);
-
-
-                       $("#loader").hide();
-                  $("#submit").show();
-                   }
-                });
-    }
-  });
-   });
-        </script>
+  </script>
 
 
 <?php
+include "inc/sidebar.php";
 include "inc/footer.php";
 ?>
