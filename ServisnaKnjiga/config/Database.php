910426cecc66854ca7faa46a922bb4e23be3c17c
StefKari <59152638+StefKari@users.noreply.github.com>
2020-04-25 22:06:51 +0200

    Update Database.php

--- ServisnaKnjiga/config/Database.php
+++ ServisnaKnjiga/config/Database.php
@@ -2,23 +2,23 @@
 
 class Database
 {
-
+    // parametri
     private $host="localhost";
     private $username="root";
     private $password="";
     private $dbName="servisna_knjiga";
 
     public $conn;
-
+    
+    // konekcija
     public function getConnection(){
-
         $this->conn = null;
 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
+        try {
+          $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName, $this->username, $this->password);
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
         }
 
         return $this->conn;
