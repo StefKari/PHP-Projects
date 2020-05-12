1e051079382f10f5b3575d3bc615dd2a726f41a5
StefKari <59152638+StefKari@users.noreply.github.com>
2020-05-13 00:07:55 +0200

    header.php

--- Monitor/admin/inc/header.php
+++ Monitor/admin/inc/header.php
@@ -25,7 +25,6 @@ $format = new Format(); // instanciranje objekta
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 </head>
 <body>
     <header id="header">
@@ -33,45 +32,45 @@ $format = new Format(); // instanciranje objekta
                 <ul calss="omot1">
                   <a class="navbar-brand" href="#"><i class="fas fa-signal"></i> Dashboard | Monitor Association of Citizens</a>
                 </ul>
-                      <?php
-                          // startovanje sesije tj logovanje
-                           if(isset($_SESSION)) {
-                             $username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
-                             $id = $_SESSION['userid'];
-                           }
-                           else {
-                             $usernam = '';
-                             $id = '';
-                           }
+                <?php
+                    // startovanje sesije tj logovanje
+                     if(isset($_SESSION)) {
+                       $username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
+                       $id = $_SESSION['userid'];
+                     }
+                     else {
+                       $usernam = '';
+                       $id = '';
+                     }
 
-                           // unistavanje sesije odnosno logout
-                          if(isset($_GET['action']) && $_GET['action'] == "logout"){
-                            Session::destroy();
-                            }
+                     // unistavanje sesije odnosno logout
+                    if(isset($_GET['action']) && $_GET['action'] == "logout"){
+                      Session::destroy();
+                      }
 
-                        ?>  <ul class="omot2">
-                                <a class="navbar-brand" >Hello <span style="color:#3c420f;"><i class="fas fa-user"></i> <?php echo $username; ?></span></a>
-                                <a class="navbar-brand" href="?action=logout">Logout</a>
-                            </ul>
-                    </nav>
+                  ?>  <ul class="omot2">
+                          <a class="navbar-brand" >Hello <span style="color:#3c420f;"><i class="fas fa-user"></i> <?php echo $username; ?></span></a>
+                          <a class="navbar-brand" href="?action=logout">Logout</a>
+                      </ul>
+            </nav>
             <div class="naslov clear">
                   <ul>
                       <li class="nav-item"><a class="nav-link" href="index.php"><i class="fas fa-user-shield"></i> Dashboard</a></li>
               				<li class="nav-item">
-                              <a class="nav-link" href="inbox.php"><i class="fas fa-inbox"></i> Inbox
-                              <?php
-                              // citanje iz baze gde su poruke sa statusom nula odnosno nisu procitane tj nije seen
-                              $query = "SELECT * FROM contact WHERE status = '0'";
-                              $msg = $database->select($query);
-                              if($msg){
-                                  echo "(".$count = mysqli_num_rows($msg).")";
-                              } else {
-                                  $count = "";
-                              }
-                              ?>
-                              </a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="http://localhost/Monitor/index.php"><i class="far fa-square"></i> Visit Website</a></li>
+                          <a class="nav-link" href="inbox.php"><i class="fas fa-inbox"></i> Inbox
+                          <?php
+                          // citanje iz baze gde su poruke sa statusom nula odnosno nisu procitane tj nije seen
+                          $query = "SELECT * FROM contact WHERE status = '0'";
+                          $msg = $database->select($query);
+                          if($msg) {
+                              echo "(".$count = mysqli_num_rows($msg).")";
+                          } else {
+                              $count = "";
+                          }
+                          ?>
+                          </a>
+                       </li>
+                       <li class="nav-item"><a class="nav-link" href="http://localhost/Monitor/index.php"><i class="far fa-square"></i> Visit Website</a></li>
                   </ul>
             </div>
     </header>
