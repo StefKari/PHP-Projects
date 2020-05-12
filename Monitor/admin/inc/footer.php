8a704e3fa078149c2bfa4dd4b71cf3d59c198974
StefKari <59152638+StefKari@users.noreply.github.com>
2020-05-13 00:07:25 +0200

    footer.php

--- Monitor/admin/inc/footer.php
+++ Monitor/admin/inc/footer.php
@@ -3,7 +3,7 @@
       <div class="container-fluid px-0">
             <div class="row no-gutters">
                 <div class="col-12">
-                  <p>Copyright &copy; <a href="https://github.com/StefKari" target="_blank">github.com/StefKari</a> All Rights Reserved.</p>
+                  <p>Copyright &copy; Stef. All Rights Reserved.</p>
                 </div>
             </div>
         </div>
